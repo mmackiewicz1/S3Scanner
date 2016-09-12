b11464eed161aead4df6f18262c387b2dc79e2fb
mmackiewicz1 <m.mackiewicz@kainos.com>
2016-09-12 15:34:28 +0200

    Added secondary algorithm

--- src/test/java/SecondaryS3ScannerTests.java
+++ src/test/java/SecondaryS3ScannerTests.java
@@ -1,17 +1,14 @@
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.services.s3.AmazonS3Client;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
 import java.io.IOException;
 import java.util.Scanner;
 
-public class S3ScannerTests {
+public class SecondaryS3ScannerTests {
     private static final long ONE_BYTE_BUFFER_SIZE = 1;
     private static final long SMALL_BUFFER_SIZE = 64;
     private static final long MEDIUM_BUFFER_SIZE = 32768;
@@ -34,7 +31,7 @@ public class S3ScannerTests {
 
     @Test
     public void When_FileContentIsBeingLoadedWithMediumBuffer_Expect_ToContainTheSameData() throws IOException {
-        S3Scanner s3Scanner = new S3Scanner(new AmazonS3Client(new ProfileCredentialsProvider()), DOMAIN, FILE_URL, MEDIUM_BUFFER_SIZE);
+        SecondaryS3Scanner s3Scanner = new SecondaryS3Scanner(DOMAIN, FILE_URL, MEDIUM_BUFFER_SIZE);
         String line;
         long startTime = System.nanoTime();
         while ((line = s3Scanner.getLine()) != null) {
@@ -46,7 +43,7 @@ public class S3ScannerTests {
 
     @Test
     public void When_FileContentIsBeingLoadedWithLargeBuffer_Expect_ToContainTheSameData() throws IOException {
-        S3Scanner s3Scanner = new S3Scanner(new AmazonS3Client(new ProfileCredentialsProvider()), DOMAIN, FILE_URL, LARGE_BUFFER_SIZE);
+        SecondaryS3Scanner s3Scanner = new SecondaryS3Scanner(DOMAIN, FILE_URL, LARGE_BUFFER_SIZE);
         String line;
         long startTime = System.nanoTime();
         while ((line = s3Scanner.getLine()) != null) {
@@ -58,7 +55,7 @@ public class S3ScannerTests {
 
     @Test
     public void When_FileContentIsBeingLoadedWithSmallBuffer_Expect_ToContainTheSameData() throws IOException {
-        S3Scanner s3Scanner = new S3Scanner(new AmazonS3Client(new ProfileCredentialsProvider()), DOMAIN, FILE_URL, SMALL_BUFFER_SIZE);
+        SecondaryS3Scanner s3Scanner = new SecondaryS3Scanner(DOMAIN, FILE_URL, SMALL_BUFFER_SIZE);
         String line;
         long startTime = System.nanoTime();
         while ((line = s3Scanner.getLine()) != null) {
@@ -70,7 +67,7 @@ public class S3ScannerTests {
 
     @Test
     public void When_FileContentIsBeingLoadedWithOne_ByteBuffer_Expect_ToContainTheSameData() throws IOException {
-        S3Scanner s3Scanner = new S3Scanner(new AmazonS3Client(new ProfileCredentialsProvider()), DOMAIN, FILE_URL, ONE_BYTE_BUFFER_SIZE);
+        SecondaryS3Scanner s3Scanner = new SecondaryS3Scanner(DOMAIN, FILE_URL, ONE_BYTE_BUFFER_SIZE);
         String line;
         long startTime = System.nanoTime();
         while ((line = s3Scanner.getLine()) != null) {
